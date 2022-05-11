c671d2b2c201ad1173e32a8945ccc6aaeb0cca13
emanuele.mellace <emanuele.mellace@nttdata.com>
2022-05-11 16:16:38 +0200

    add tests

--- src/main/java/workshop/mellace/testProject/controller/HelloWorldController.java
+++ src/main/java/workshop/mellace/testProject/controller/HelloWorldController.java
@@ -23,5 +23,52 @@ public class HelloWorldController {
 		return "<h2> test </h2>";
 	}
 	
+	@RequestMapping("/test1")  
+	public String test1()   
+	{  
+		return "<h2> test1 </h2>";
+	}
+	
+	@RequestMapping("/test2")  
+	public String test2()   
+	{  
+		return "<h2> test2 </h2>";
+	}
+	
+	@RequestMapping("/test3")  
+	public String test3()   
+	{  
+		return "<h2> test3 </h2>";
+	}
+	
+	@RequestMapping("/test4")  
+	public String test4()   
+	{  
+		return "<h2> test4 </h2>";
+	}
+	
+	@RequestMapping("/test5")  
+	public String test5()   
+	{  
+		return "<h2> test5 </h2>";
+	}
+	
+	@RequestMapping("/test6")  
+	public String test6()   
+	{  
+		return "<h2> test6 </h2>";
+	}
+	
+	@RequestMapping("/test7")  
+	public String test7()   
+	{  
+		return "<h2> test7 </h2>";
+	}
+	
+	@RequestMapping("/test8")  
+	public String test8()   
+	{  
+		return "<h2> test8 </h2>";
+	}
 	
 }
--- src/test/java/workshop/mellace/controller/ControllerTest.java
+++ src/test/java/workshop/mellace/controller/ControllerTest.java
@@ -25,4 +25,48 @@ public class ControllerTest {
 		HelloWorldController controller = new HelloWorldController();
 		assertEquals("<h2> test </h2>",controller.test());
 	}
+	
+	@Test
+	public void testTest1() {
+		HelloWorldController controller = new HelloWorldController();
+		assertEquals("<h2> test1 </h2>",controller.test1());
+	}
+	
+	@Test
+	public void testTest2() {
+		HelloWorldController controller = new HelloWorldController();
+		assertEquals("<h2> test2 </h2>",controller.test2());
+	}
+	
+	@Test
+	public void testTest3() {
+		HelloWorldController controller = new HelloWorldController();
+		assertEquals("<h2> test3 </h2>",controller.test3());
+	}
+	
+	@Test
+	public void testTest4() {
+		HelloWorldController controller = new HelloWorldController();
+		assertEquals("<h2> test4 </h2>",controller.test4());
+	}
+	
+	@Test
+	public void testTest6() {
+		HelloWorldController controller = new HelloWorldController();
+		assertEquals("<h2> test6 </h2>",controller.test6());
+	}
+	
+	@Test
+	public void testTest7() {
+		HelloWorldController controller = new HelloWorldController();
+		assertEquals("<h2> test7 </h2>",controller.test7());
+	}
+	
+	@Test
+	public void testTest8() {
+		HelloWorldController controller = new HelloWorldController();
+		assertEquals("<h2> test8 </h2>",controller.test8());
+	}
+
+	
 }
